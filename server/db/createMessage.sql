28dce3ad3aec2c2fed4d9e587d9b05af440b41bb
= <=>
2020-08-15 08:53:15 -0400

    change createMessage.sql

--- server/db/createMessage.sql
+++ server/db/createMessage.sql
@@ -1,4 +1,17 @@
 INSERT INTO Messages
   (user_id, body, channel_id)
 VALUES($1, $2, $3)
-RETURNING *;
+
+select m.body, m.channel_id, m.id, c.name, u.username
+from messages m
+  join channels c on c.id = m.channel_id
+  join users u on u.id = m.user_id
+where m.channel_id
+in (select cm.channel_id
+from channelMember cm
+where cm.user_id = 1)
+order by m.id desc limit 1;
+
+
+
+
